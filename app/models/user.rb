edade68995bc1403734407aadf939fd0d1640941
chrisjamesr <chrisjames.r@gmail.com>
2018-01-30 13:06:24 -0500

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :recipes
 
   has_secure_password
-  validates :name, :allow_blank => false
-  validates :password, :length => { in: 6..20 }
+  validates :name, presence: true, allow_blank: false
+  validates :password, presence: true, length: { in: 6..20 }
 
 end
