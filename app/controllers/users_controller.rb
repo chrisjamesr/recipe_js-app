0af198c535a12aed6bbfafaafeba4d04c09e9ca1
chrisjamesr <chrisjames.r@gmail.com>
2018-01-30 13:18:31 -0500

    build out create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      redirect_to user_path(@user), :notice => "Welcome #{@user.name.capitalize}" 
+    if @user.save 
       session[:user_id] = @user.id
-    else
+      redirect_to user_path(@user), :notice => "Welcome #{@user.name.capitalize}" 
+    else      
+      flash[:alert] = @user.errors.full_messages
       render :new
     end
-
   end
 
   def show
