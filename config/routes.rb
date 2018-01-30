898a55d136c80e6212e649d3effbc931b86f9c91
chrisjamesr <chrisjames.r@gmail.com>
2018-01-29 22:30:05 -0500

    add sessions routes, oauth callback, user resources with nested recipe routes and ingredient resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  post '/logout' => 'session#destroy'
+  
+  get '/auth/:provider/callback', to: 'sessions#create'
+
+  resources :user do
+    resources :recipe 
+  end
+
+  resources :ingredient
+
+  # root
 end
