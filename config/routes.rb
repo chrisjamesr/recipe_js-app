266f35842243bacb399da230a5977d428591dbb7
chrisjamesr <chrisjames.r@gmail.com>
2018-01-30 20:47:54 -0500

    add root route and modify logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  get '/login' => 'session#new'
-  post '/login' => 'session#create'
-  post '/logout' => 'session#destroy'
-  
   get '/auth/:provider/callback', to: 'sessions#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+  
+  
 
   resources :users do
     resources :recipes 
