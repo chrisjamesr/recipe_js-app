44566064ca5334187dccac0c8d808625f84fdb89
chrisjamesr <chrisjames.r@gmail.com>
2018-01-30 21:17:51 -0500

    build create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    @user = User.find_by(:name => params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to @user, :notice => "Welcome #{@user.name.capitalize}"
+    else
+      flash[:alert] = @user.errors.full_messages
+      render :new
+    end    
   end
 
   def destroy
