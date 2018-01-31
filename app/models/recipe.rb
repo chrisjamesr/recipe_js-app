40c86431c46b7a7ca34f269478f76ff7d87ea8b9
chrisjamesr <chrisjames.r@gmail.com>
2018-01-31 13:13:00 -0500

    add associations to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,12 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :ingredients
+  belongs_to :category
+
+  has_many :recipe_ingredients
+  has_many :ingredients, :through => :recipe_ingredients
+
+  # has_many :recipe_categories
+  # has_many :categories, :through => :recipe_categories
 
   
 end
