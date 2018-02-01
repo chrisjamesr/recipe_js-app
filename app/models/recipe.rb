5481671fb1f6c56685328119ca2845ee6883fddd
chrisjamesr <chrisjames.r@gmail.com>
2018-02-01 13:03:05 -0500

    add additional validation to model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, :through => :recipe_ingredients
 
+  validates :title, presence: true, allow_blank: false
   validates :category_id, presence: true, allow_blank: false
 
   # has_many :recipe_categories
