6e4dd95abfbcb77eb3e56056a037d3bd0b0a11bc
chrisjamesr <chrisjames.r@gmail.com>
2018-02-01 13:25:21 -0500

    add actions to be build out

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,13 @@
 class IngredientsController < ApplicationController
+
+  #create ingredient from recipe page 
+  def create
+
+  end
+
+  #show list of recipes using an ingredient
+  def show
+
+  end
+
 end
