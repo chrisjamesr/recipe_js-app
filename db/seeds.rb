bf8914d9153e6b148e17370c91b8381a0aa96343
chrisjamesr <chrisjames.r@gmail.com>
2018-01-31 19:55:38 -0500

    run migration, add category id to recipe

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do 
+  User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Number.number(8)
+  )
+end
+
+User.all.each do |user|
+  user.recipes.build ()  
+end
+
+FOOD_CATEGORIES = ['Appetizers', 'Desserts', 'Holiday Foods', 'Meat' 'Entres', 'Vegeterian', 'Vegan']
+FOOD_CATEGORIES.each do |category|
+    Category.create(title: category)
+end
+
+
+# counter = 1
+# Item.all.each do |item|
+#   item.category_id = counter
+#   item.save
+#   counter += 1
+# end
+
+# # User.create(name: 'Alex', email:'alex@home.com')
+# 10.times do 
+#   User.create(
+#   :name => Faker::Name.name,    
+#   :password => Devise.friendly_token.first(8),
+#   :email => Faker::Internet.email 
+#   ) 
+# end
+
+# User.all do |user|
+#   Cart.create(:user => user) do |cart|
+#     cart.line_items.build(:item => Item.first, :quantity =>1)
+#   end
+# end
