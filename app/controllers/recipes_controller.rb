bfbb234285db6cb03c9cc2a2b1bb98dd35305b54
chrisjamesr <chrisjames.r@gmail.com>
2018-02-01 13:25:29 -0500

    add actions to be build out

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,33 @@
 class RecipesController < ApplicationController
 
+  def new
+
+  end
+
+  def create
+
+  end
+
+  #show all recipes from all users or all recipes from single user
+  def index
+
+  end
+
+  #show all recipe ionformation
+  def show
+
+  end
+
+  def edit
+
+  end
+
+# protected methods
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
