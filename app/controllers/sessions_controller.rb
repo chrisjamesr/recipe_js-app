a648aa625bd09ed2c8adee33f48a3c49dd9c916f
chrisjamesr <chrisjames.r@gmail.com>
2018-01-31 22:46:46 -0500

    add logic for omniauth login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,27 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(:name => params[:user][:name])
-    if @user && @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect_to @user, :notice => "Welcome #{@user.name.capitalize}"
+    if auth
+      @user = User.find_by(:email => auth[:info][:email])
+      if @user
+        session[:user_id] = @user.id
+        redirect_to @user, :notice => "Welcome #{@user.name.capitalize}"
+      else
+        @user = User.create(:email => auth[:info][:email]) do |user|
+          user.name = auth[:info][:name]
+          user.password = Password.pronounceable(10) 
+        end
+        session[:user_id] = @user.id
+      end
     else
-      flash[:alert] = @user.errors.full_messages
-      render :new
+      @user = User.find_by(:name => params[:user][:name])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to @user, :notice => "Welcome #{@user.name.capitalize}"
+      else
+        flash[:alert] = @user.errors.full_messages
+        render :new
+      end
     end    
   end
 
@@ -20,4 +34,10 @@ class SessionsController < ApplicationController
     flash[:notice] = "You have been logged out!"
     redirect_to root_path
   end
+
+  private
+  def auth 
+    request.env['omniauth.auth']
+  end
+
 end
