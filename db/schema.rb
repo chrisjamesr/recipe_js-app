2daf98883f50492586f7b8989adbe837bc3425ac
chrisjamesr <chrisjames.r@gmail.com>
2018-01-31 22:35:16 -0500

    add category_id to recipe table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131175425) do
+ActiveRecord::Schema.define(version: 20180131180818) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20180131175425) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
