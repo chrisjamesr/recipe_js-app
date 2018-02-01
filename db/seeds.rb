5a11ab1f3a0baeaacee2bb4d060816dac8770098
chrisjamesr <chrisjames.r@gmail.com>
2018-02-01 13:04:40 -0500

    fix typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 FOOD_CATEGORIES = ['Appetizers', 'Desserts', 'Holiday Foods', 'Meat' 'Entres', 'Vegeterian', 'Vegan']
+
 FOOD_CATEGORIES.each do |category|
     Category.create(name: category)
 end
@@ -21,8 +22,8 @@ end
 
 User.all.each do |user|
   user.recipes.create(
-    :title => Faker::Food.dish
-    :category_id => Category.all.sample
+    :title => Faker::Food.dish,
+    :category_id => Category.all.sample,
     :description => Faker::Food.description
     )  
 end
