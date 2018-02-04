d06e1ff6592375e8b66c545325014ddb0097cc66
chrisjamesr <chrisjames.r@gmail.com>
2018-02-04 14:53:24 -0500

    commend out validation

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -2,6 +2,6 @@ class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
   
-  validates :recipe_id, :ingredient_id, presence: true, allow_blank: false
+  # validates :recipe_id, :ingredient_id, presence: true, allow_blank: false
   
 end
