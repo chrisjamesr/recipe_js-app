d2145d751812201106d41c25eea4a3767ad7bb12
chrisjamesr <chrisjames.r@gmail.com>
2018-02-03 20:50:11 -0500

    modify associations for many to many relationship:

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,5 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  belongs_to :category
 
   has_many :recipe_ingredients
   has_many :ingredients, :through => :recipe_ingredients
@@ -8,8 +7,9 @@ class Recipe < ApplicationRecord
   validates :title, presence: true, allow_blank: false
   validates :category_id, presence: true, allow_blank: false
 
-  # has_many :recipe_categories
-  # has_many :categories, :through => :recipe_categories
+  belongs_to :category
+  has_many :recipe_categories
+  has_many :categories, :through => :recipe_categories
 
   def ingredient_attributes
 
