e73d4c41e3dff6e531e7d074f0abeff082071336
chrisjamesr <chrisjames.r@gmail.com>
2018-02-05 13:19:59 -0500

    modify ingredients attributes method to correctly add new recipe_ingredients attributes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -13,17 +13,23 @@ class Recipe < ApplicationRecord
   has_many :categories, :through => :recipe_categories
 
   def ingredients_attributes=(ingredients_attributes)
-
     ingredients_attributes.each do |i, ingredient_attribute|
       if ingredient_attribute.present?
-        ingredient = self.ingredients.build(:name => ingredient_attribute[:name]) 
+        ingredient = Ingredient.find_or_create_by(:name => ingredient_attribute[:name]) 
         if !self.ingredients.include?(ingredient)
           self.recipe_ingredients.build(
             :ingredient => ingredient,
             :quantity => ingredient_attribute[:recipe_ingredients][:quantity], 
-            :direction => ingredient_attribute[:recipe_ingredients][:direction]
-            )          
-        end
+            :preparation => ingredient_attribute[:recipe_ingredients][:preparation]
+          )     
+        # binding.pry   
+        # else
+        #   recipe_ingredient = self.recipe_ingredients.find_by(:ingredient_id => ingredient.id, :recipe_id => self.id)
+        #   recipe_ingredient.update(
+        #     :quantity => ingredient_attribute[:recipe_ingredients][:quantity], 
+        #     :preparation => ingredient_attribute[:recipe_ingredients][:preparation]
+        #     )
+        end 
       end
     end    
   end
