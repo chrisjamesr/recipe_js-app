13672a4932e219ac137b8cf39c10bc73b3a4bf18
chrisjamesr <chrisjames.r@gmail.com>
2018-02-07 18:07:59 -0500

    add has_permission? method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,15 @@ class ApplicationController < ActionController::Base
   def set_session
     session[:user_id] = @user.id if @user
   end
-end
+
+  def has_permission?
+    user = User.find_by(:id => params[:user_id])
+    recipe = Recipe.find_by(:id => params[:id])
+    unless current_user == user && current_user.recipes.include?(recipe)
+      flash[:notice] = "You are not authorized to edit this record!"
+      redirect_to user_recipe_path(user, recipe)
+    end
+  end
+
+end  # End of Class
+
