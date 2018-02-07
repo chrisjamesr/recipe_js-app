dd96c77e26d30ce5129c714435d3f8611c1fc8a7
chrisjamesr <chrisjames.r@gmail.com>
2018-02-07 13:19:18 -0500

    fix indentation

--- app/helpers/recipe_ingredients_helper.rb
+++ app/helpers/recipe_ingredients_helper.rb
@@ -3,8 +3,9 @@ module RecipeIngredientsHelper
   def recipe_ingredient_new_or_edit(recipe, ingredient)
     if recipe.new_record? 
       recipe.recipe_ingredients.build 
-    elsif recipe_ingredient = recipe.recipe_ingredients.find_by(:recipe_id => recipe.id, :ingredient_id => ingredient.id)
+    else recipe_ingredient = recipe.recipe_ingredients.find_by(:recipe_id => recipe.id, :ingredient_id => ingredient.id)
       recipe_ingredient
     end
   end
-end
+  
+end  # End of Class
