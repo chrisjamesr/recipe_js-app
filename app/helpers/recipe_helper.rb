d9e9cfa146772bb82ebc1984b6f1ee94043e24de
chrisjamesr <chrisjames.r@gmail.com>
2018-02-07 17:53:46 -0500

    rename edit link helper method

--- app/helpers/recipe_helper.rb
+++ app/helpers/recipe_helper.rb
@@ -4,7 +4,7 @@ module RecipeHelper
     recipe.new_record? ? recipe.ingredients.build : recipe.ingredients
   end
 
-  def show_edit_and_delete_buttons
+  def show_edit_link
     if current_user == User.find(params[:user_id])
       link_to "Edit", edit_user_recipe_path(current_user, Recipe.find(params[:id]))
       # button_to "Delete", user_recipe_path(current_user, Recipe.find(params[:id])), :method => "delete"
