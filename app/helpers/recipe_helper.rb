dd9591cfae1cd5b4f1991559d430229d6082eca5
chrisjamesr <chrisjames.r@gmail.com>
2018-02-07 13:18:27 -0500

    add link to edit for current_user's recipes

--- app/helpers/recipe_helper.rb
+++ app/helpers/recipe_helper.rb
@@ -3,4 +3,11 @@ module RecipeHelper
   def recipe_new_or_edit(recipe)
     recipe.new_record? ? recipe.ingredients.build : recipe.ingredients
   end
-end
+
+  def show_edit_and_delete_buttons
+    if current_user == User.find(params[:user_id])
+      link_to "Edit", edit_user_recipe_path(current_user, Recipe.find(params[:id]))
+      # button_to "Delete", user_recipe_path(current_user, Recipe.find(params[:id])), :method => "delete"
+    end
+  end
+end  # End of
