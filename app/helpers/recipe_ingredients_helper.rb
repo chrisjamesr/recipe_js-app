3b8b8f138878655e68c7020bab1c6ac473301e8a
chrisjamesr <chrisjames.r@gmail.com>
2018-02-08 13:29:23 -0500

    modify #recipe_ingredient_new_or_edit to handle existing recipe with no ingredients

--- app/helpers/recipe_ingredients_helper.rb
+++ app/helpers/recipe_ingredients_helper.rb
@@ -3,9 +3,17 @@ module RecipeIngredientsHelper
   def recipe_ingredient_new_or_edit(recipe, ingredient)
     if recipe.new_record? 
       recipe.recipe_ingredients.build 
-    else recipe_ingredient = recipe.recipe_ingredients.find_by(:recipe_id => recipe.id, :ingredient_id => ingredient.id)
+    elsif recipe_ingredient = recipe.recipe_ingredients.find_by(:recipe_id => recipe.id, :ingredient_id => ingredient.id)
       recipe_ingredient
+    elsif recipe.persisted? && recipe.recipe_ingredients.empty?
+      recipe_ingredient = RecipeIngredient.new(:recipe_id => recipe.id)
     end
   end
   
+  def remove_recipe_ingredient(recipe_ingredient_object)
+    if request.parameters[:action] == "edit" && @recipe.recipe_ingredients.present?
+      link_to "x", remove_ingredient_path(recipe_ingredient_object), :method => "delete"
+    end
+  end
+
 end  # End of Class
