adb6d34ea68bfb009a9b99d212256dd3e1be9022
chrisjamesr <chrisjames.r@gmail.com>
2018-02-07 21:43:46 -0500

    add helper methods nested_route?, curent_user_path? and display_user_name

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,18 @@
 module UserHelper
+
+  def nested_route?
+    request.parameters.keys.include?("user_id")
+  end
+
+  def current_user_path?
+      request.original_fullpath == user_recipes_path(current_user)
+  end
+
+  def display_user_name
+    if nested_route?
+      @user = User.find_by(:id => request.parameters[:user_id]) 
+      @user.name
+    end
+  end
+
 end
