4917dd35d78cd3b9bf067561d327b57c9afe4224
chrisjamesr <chrisjames.r@gmail.com>
2018-02-10 09:41:08 -0500

    add .filter_options method to call class methods for filtering, add filter methods for category, ingredient and both

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,12 +3,9 @@ class Recipe < ApplicationRecord
 
   has_many :recipe_ingredients, :dependent => :destroy
   has_many :ingredients, :through => :recipe_ingredients
-
   validates :title, :description, :directions, presence: true, allow_blank: false
   validates :title, uniqueness: true
-  
-
-  # belongs_to :category
+    # belongs_to :category
   has_many :recipe_categories
   has_many :categories, :through => :recipe_categories
 
@@ -42,5 +39,31 @@ class Recipe < ApplicationRecord
       end
     end
   end
+
+   def self.filter_options(params)
+    if params[:ingredient_id].nil? && params[:category_id].nil?
+      Recipe.all
+    elsif params[:ingredient_id].present? && params[:category_id].present?
+      filter_by_category_and_ingredient(params[:ingredient_id], params[:category_id])
+    elsif params[:category_id].present? && params[:ingredient_id].empty?
+      filter_by_category(params[:category_id])
+    elsif params[:ingredient_id].present? && params[:category_id].empty?
+      filter_by_ingredient(params[:ingredient_id])
+    end
+  
+  end
+
+  def self.filter_by_category(category_id)
+    joins(:recipe_categories).where("recipe_categories.category_id = ?", category_id)
+  end
+
+   def self.filter_by_ingredient(ingredient_id)
+    joins(:recipe_ingredients).where("recipe_ingredients.ingredient_id = ?", ingredient_id)
+  end
+
+   def self.filter_by_category_and_ingredient(category_id, ingredient_id)
+    joins(:recipe_ingredients, :recipe_categories).where("recipe_ingredients.ingredient_id = ? AND recipe_categories.category_id = ?", ingredient_id, category_id)
+  end
+
 
 end
