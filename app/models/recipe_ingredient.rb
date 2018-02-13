8436b54fba1489d080f57dbe46b8f66576d913cc
chrisjamesr <chrisjames.r@gmail.com>
2018-02-12 21:57:35 -0500

    add custom validation to prevent duplicate seed data

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,7 +1,13 @@
 class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
-  
+  validate :recipe_ingredient_unique
   # validates :recipe_id, :ingredient_id, presence: true, allow_blank: false
+
   
+  def recipe_ingredient_unique
+    if RecipeIngredient.where(recipe_id: self.recipe_id, ingredient_id: self.ingredient_id).exists?
+      self.errors[:base] << "This ingredient already exists"
+    end
+  end
 end
