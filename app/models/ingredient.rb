fd02d7a8100be033f2a29d13bbed152bcea2ecc2
chrisjamesr <chrisjames.r@gmail.com>
2018-02-12 21:58:10 -0500

    add uniqueness validation on name

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,5 @@ class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, :through => :recipe_ingredients
 
-  validates :name, presence: true, allow_blank: false
+  validates :name, presence: true, allow_blank: false, :uniqueness => {:case_sensitive => false}
 end
