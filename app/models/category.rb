567cd8ad1dc71e5ed3268f3f202a9f49789cabfb
chrisjamesr <chrisjames.r@gmail.com>
2018-02-12 21:23:18 -0500

    add validation for category name uniqueness

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,5 +4,6 @@ class Category < ApplicationRecord
   has_many :recipe_categories
   has_many :recipes, :through => :recipe_categories
 
-  validates :name, presence: true, allow_blank: false
+  validates :name, :presence => true, :allow_blank => false, :uniqueness => {:case_sensitive => false}
+
 end
