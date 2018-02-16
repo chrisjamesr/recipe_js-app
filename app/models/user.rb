f46657d0c786a09b3f6bd2b4ccbc4af588c5faf7
chrisjamesr <chrisjames.r@gmail.com>
2018-02-16 13:04:27 -0500

    add methods to add or create & log in user from omni auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User < ApplicationRecord
   validates :name, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: { in: 6..20 }
 
+  def self.login_from_omniauth(auth)
+    find_from_omniauth(auth) || create_from_omniauth(auth)
+  end
+
+  def self.find_from_omniauth(auth)
+    find_by(:email => auth[:info][:email])
+  end
+
+  def self.create_from_omniauth(auth)
+    create(:email => auth[:info][:email],
+      :name => auth[:info][:name],
+      :password => Password.pronounceable(10)) 
+  end
+
 end
