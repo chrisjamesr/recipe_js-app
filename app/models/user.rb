50ba593f4b7b13e6cb45fe7a024997688154bb52
chrisjamesr <chrisjames.r@gmail.com>
2018-02-15 21:38:05 -0500

    add validation for unique name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :name, presence: true, allow_blank: false
+  validates :name, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: { in: 6..20 }
 
 end
