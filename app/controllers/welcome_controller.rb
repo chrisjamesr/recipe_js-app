4e966cb203dba6a25c1a52b58c034aff15e78033
chrisjamesr <chrisjames.r@gmail.com>
2018-02-20 23:09:05 -0500

    add before_action set_session_filter, add set_session_filter method, change redirect paths

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,18 @@
 class WelcomeController < ApplicationController
+  before_action :set_session_filter, :only => [:home]
   
   def home
     if logged_in?
       @user = current_user
-      redirect_to user_path(@user)
+      redirect_to user_recipes_path(@user)
+    else
+      redirect_to login_path
     end
   end
+
+  private
+    def set_session_filter
+      session[:filter_params] = {}
+    end
+
 end
