9db057482d2e7e185c0d687ec5281aaacbf2553e
chrisjamesr <chrisjames.r@gmail.com>
2018-02-20 23:03:27 -0500

    add if_user_path method to add hidden field if user id is present

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def if_user_path
+    if params[:user_id].present?
+      tag("input", :type => "hidden", :name=> "user_id",:value => params[:user_id])
+    end
+  end
+
 end
