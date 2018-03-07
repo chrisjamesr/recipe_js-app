9d3bda0144cc4b480c42ebc2601bad6d517b5e65
chrisjamesr <chrisjames.r@gmail.com>
2018-03-07 18:35:05 -0500

    add helpermethod to display svg icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
     end
   end
 
+  def svg(name)
+    # raise name.inspect
+    file_path = "#{Rails.root}/app/assets/images/#{name}.svg"
+    return File.read(file_path).html_safe if File.exists?(file_path)
+    '(not found)'
+  end
+
 end
