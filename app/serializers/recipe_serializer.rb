af8c974e120917aa4c85178842132c9f6f26cbca
chrisjamesr <chrisjames.r@gmail.com>
2018-04-22 12:43:54 -0400

    add association to return user data via user serializer

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,3 +1,4 @@
 class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :title, :time, :user_id, :description, :directions, :created_at
+  belongs_to :user, serializer: UserSerializer
 end
