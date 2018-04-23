b24997201dbd1df0a4976aada5a7bce29869ccc0
chrisjamesr <chrisjames.r@gmail.com>
2018-04-22 22:53:58 -0400

    relocate user constructor. build out recipeIngredients constructor. add function for handlebars implementation(notworking)

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -18,8 +18,7 @@ function loadIndexedRecipes(tag){
     .done(function(response){
       response.forEach(function(element){
        // new User.find(element.user)
-       new Recipe(element)
-        
+       new Recipe(element)        
     })
     displayIndexedRecipes()    
   })
@@ -37,20 +36,10 @@ function displayIndexedRecipes(){
 }
 
 function displayRecipe(recipe){
-  let recipeTemplateString = `<div class="card">
-  <div class="card-body">
-    <h5 class="card-title"><%= link_to ${recipe.title}, user_recipe_path(recipe.user, recipe) %></h5>
-     <div class="category-tag">
-      <%= render :partial => "categories/categories", :collection => recipe.categories, :as => :category %>
-    </div>
-    <h6 class="card-subtitle mb-2 text-muted">Submitted by <%= link_to ${recipe.user.name}, user_recipes_path(recipe.user),class: "js-user-link" %></h6>
-    <em>Approximate Cook Time: ${recipe.time} Min</em><br>
-    <%= recipe.description %>
-   
-  </div>
-</div>  
-  <br>`
-  // debugger
+  debugger
+  let recipeTemplate = $('#recipe-template').html()
+  let recipeTemplateString = Handlebars.compile(recipeTemplate);
+
   $('#recipe-cards').append(recipeTemplateString)
 }
 
@@ -79,15 +68,7 @@ function loadRecipeText(res){
   $('#js-recipe-title').data().recipeId = res["id"]
   
 }
-// {
-//   created_at: "2018-04-18T01:24:09.009Z"
-//   description:  "Fresh parsley, Italian sausage, shallots, garlic, sun-dried tomatoes and mozzarella cheese in an all-butter crust. With a side of mixed fruits."
-//   directions: "Girl, if God created anything less beautiful than you, I hope He kept it for Himself."
-//   id:36
-//   time: 6
-//   title:   "Vegetable Soup"
-//   user_id: 16
-// }
+
 
 function createRecipe(){
   let recipes = []
@@ -99,6 +80,7 @@ function createRecipe(){
       this.title = response.title
       this.user = User.findOrCreateUser(response.user)
       this.directions = response.directions
+      this.recipeIngredients = addRecipeIngredients(response.recipe_ingredients)
       recipes.push(this);
       this.user.addRecipe(this);
     }
@@ -109,28 +91,45 @@ function createRecipe(){
 }
 const Recipe = new createRecipe
 
-function createUser(){
-  let users = []
-  return class User{
-    constructor(responseUser){
-      this.id = responseUser.id
-      this.name = responseUser.name
-      this.recipes = []
-      users.push(this)
-    }
-    static recipes(){
-      return this.recipes
-    }
-    static all(){
-      return users
-    }
-    addRecipe(recipe){
-      this.recipes.push(recipe);
-    }
-    static findOrCreateUser(userObj){
-      return users.find(user=> user.id === userObj.id && user.name === userObj.name) || new User(userObj) 
+function createRecipeIngredient(){
+  return class RecipeIngredient{
+    constructor(recipeIngredientObject){
+      this.id = recipeIngredientObject.id
+      this.recipeId = recipeIngredientObject.recipe_id
+      this.ingredientId = recipeIngredientObject.ingredient_id
+      this.quantity = recipeIngredientObject.quantity
+      this.preparation = recipeIngredientObject.preparation
+
     }
   }
 }
+const RecipeIngredient = new createRecipeIngredient
 
-const User = new createUser
+function addRecipeIngredients(recipeIngredientsArray){
+  recipeIngredientsArray.forEach(item => new RecipeIngredient(item))
+}
+// function createUser(){
+//   let users = []
+//   return class User{
+//     constructor(responseUser){
+//       this.id = responseUser.id
+//       this.name = responseUser.name
+//       this.recipes = []
+//       users.push(this)
+//     }
+//     static recipes(){
+//       return this.recipes
+//     }
+//     static all(){
+//       return users
+//     }
+//     addRecipe(recipe){
+//       this.recipes.push(recipe);
+//     }
+//     static findOrCreateUser(userObj){
+//       return users.find(user=> user.id === userObj.id && user.name === userObj.name) || new User(userObj) 
+//     }
+//   }
+// }
+
+// const User = new createUser
