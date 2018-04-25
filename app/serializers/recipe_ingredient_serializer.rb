8f50a4f10def6eae1e46f91075face1db365a228
chrisjamesr <chrisjames.r@gmail.com>
2018-04-25 12:45:44 -0400

    add ingredient association

--- app/serializers/recipe_ingredient_serializer.rb
+++ app/serializers/recipe_ingredient_serializer.rb
@@ -1,5 +1,6 @@
 class RecipeIngredientSerializer < ActiveModel::Serializer
   attributes :id, :recipe_id, :ingredient_id, :quantity, :preparation
   belongs_to :recipe
+  belongs_to :ingredient, serializer: IngredientSerializer
 end
 
