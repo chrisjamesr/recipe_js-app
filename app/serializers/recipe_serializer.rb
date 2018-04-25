89fb9ee5abeb129792b609da5c7a227c7eb015f3
chrisjamesr <chrisjames.r@gmail.com>
2018-04-25 12:46:21 -0400

    add ingredient hmt association

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -2,4 +2,5 @@ class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :title, :time, :user_id, :description, :directions, :created_at
   belongs_to :user, serializer: UserSerializer
   has_many :recipe_ingredients, serializer: RecipeIngredientSerializer
+  has_many :ingredients, through: :recipe_ingredients
 end
