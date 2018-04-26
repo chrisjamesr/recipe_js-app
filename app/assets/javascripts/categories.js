d5459966c9dcceb0dc8cf88be9735944ff4d6e39
chrisjamesr <chrisjames.r@gmail.com>
2018-04-25 21:58:25 -0400

    begin build out category class

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,3 +1,17 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+
+function createCategory(){
+  let categories = []
+  return class Category{
+    constructor(categoryObject){
+      this.id = categoryObject.id
+      this.name = categoryObject.name
+      categories.push(this)
+    }
+    static findOrCreateCategory(){
+      
+    }
+  }
+}
