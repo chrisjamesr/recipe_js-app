50f640537e3f66e62e4647d1e783574ab62df656
chrisjamesr <chrisjames.r@gmail.com>
2018-04-25 22:17:00 -0400

    add custom validation to prevent duplicate associations

--- app/models/recipe_category.rb
+++ app/models/recipe_category.rb
@@ -1,4 +1,13 @@
 class RecipeCategory < ApplicationRecord
   belongs_to :category
   belongs_to :recipe
+  validate :recipe_category_unique
+  # validates :recipe_id, :ingredient_id, presence: true, allow_blank: false
+
+  
+  def recipe_category_unique
+    if RecipeCategory.where(recipe_id: self.recipe_id, category_id: self.category_id).exists? && self.new_record?
+      self.errors[:base] << "This association already exists"
+    end
+  end
 end
