5c1d8fdf910f4ac6d5a67de814b48869724db220
chrisjamesr <chrisjames.r@gmail.com>
2018-04-29 08:29:07 -0400

    use correct array protoype method to return categories array for findOrCreateCategories

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -4,14 +4,24 @@
 
 function createCategory(){
   let categories = []
+
   return class Category{
     constructor(categoryObject){
       this.id = categoryObject.id
       this.name = categoryObject.name
       categories.push(this)
     }
-    static findOrCreateCategory(){
-      
+
+    static all(){
+      return categories
+    }
+
+    static findOrCreateCategories(categoriesArray){
+      return categoriesArray.map(function(category){
+        return (Category.all().find(c=> c.id === category.id) || new Category(category) )     
+      })
     }
   }
+
 }
+const Category = new createCategory
