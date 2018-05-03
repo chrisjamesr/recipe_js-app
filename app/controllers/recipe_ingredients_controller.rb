0a00a1ee16d877786497f60119b1d385932bf0e7
chrisjamesr <chrisjames.r@gmail.com>
2018-05-03 13:28:21 -0400

    removed unused code

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -10,7 +10,7 @@ class RecipeIngredientsController < ApplicationController
     @recipe = @recipe_ingredient.recipe
     @recipe_ingredient.destroy
     respond_to do |format|
-      format.html { render edit_user_recipe_path(@recipe.user.id, @recipe.id)}
+      format.html { render layout: false}
       format.json { render json: @recipe_ingredient,  status: 200}
     end
   end
