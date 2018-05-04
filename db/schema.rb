97dd5009418dca48c94d16146b0d44f984c08aab
chrisjamesr <chrisjames.r@gmail.com>
2018-05-04 13:07:14 -0400

    update schema after new db migration:

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205033115) do
+ActiveRecord::Schema.define(version: 20180504165939) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.integer "recipe_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
