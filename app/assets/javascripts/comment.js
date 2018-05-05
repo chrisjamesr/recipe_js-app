0a31104fe6da90e142af8b506f0514e6c4aa73ba
chrisjamesr <chrisjames.r@gmail.com>
2018-05-05 19:47:29 -0400

    modify showComments to add comments individually, refactored to use displayComment. Comment field remains after comment posted, input clears

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -5,8 +5,8 @@ $(document).on('turbolinks:load', function(){
 
 function addCommentEventListeners(){
   $("#show-comments").on('click', loadComments)
-  $("#add-comment").on('click', addCommentField)
 }
+
 
 function addCommentField(){
   event.stopPropagation()
@@ -21,26 +21,25 @@ function addCommentField(){
     "cols": "70",
     "wrap": "hard"
   })
-  let $commentButton = $("<button>Submit</button>", {
+  let $commentButton = $("<button>", {
+    text: "Submit",
     "value": "Submit",
-    "type": "button",
-  })
-  $commentButton.on('click', postComment)
-  // $('#comments')
+    "type": "button"
+  }).on('click', postComment)
+
   let $commentForm = $("<form>", {
     "id": "js-comment-form"
     })
   let $addCommentDiv = $("<div></div>",{
     "id": "js-comment-div"
   }).append($commentField,[$commentButton])
-  
-  $($addCommentDiv).insertBefore($('#comments'))
 
+  $($addCommentDiv).insertBefore($('#comments'))
 }
 
 function loadComments(){
   event.stopPropagation()
-  const recipeId = this.dataset.recipeId
+  let recipeId = $('#js-recipe-title').data().recipeId
   $.ajax({
       method: "GET",
       url: `/recipes/${recipeId}/comments`,
@@ -48,24 +47,69 @@ function loadComments(){
     }).done(showComments)
 }
 
+function showComments(commentsResponse){
+  let $commentTemplate = $('<div>', { "id": "comments"})
+  if (commentsResponse.length === 0){
+    addCommentField()
+  } else {  
+    commentsResponse.forEach(function(comment){
+      // add each comment individually 
+      prependComment(comment)
+      // let userLink = `<a href="/users/${comment.user_id}/recipes">${comment.user_name}</a>`
+      // let $commentString = $(`<p>${userLink}<br>${comment.text}</p>`)    
+      // $('#comments').prepend($commentString)
+    })
+    // $('#comments').append($commentTemplate)
+    addCommentField()
+   
+  }
+}
+
 function postComment(){
-  alert('postComment')
-  // const commentText;
-  // const url;
+  let commentText = $('#js-comment-input').val().trim()
+  let recipeId = $('#js-recipe-title').data().recipeId
   $.ajax({
     method: "POST",
-    url: '/recipes/4/comments',
-    data: {text: ""},
+    url: `/recipes/${recipeId}/comments`,
+    data: {text: commentText},
     dataType: 'json'
   }).done(prependComment)
-    
+  // $('#js-comment-div').remove()  
+  $('#js-comment-input').val('')  
 }
 
-function showComments(commentsResponse){
-  debugger
-  console.log(commentsResponse)
+function prependComment(comment){
+  let userLink = `<a href="/users/${comment.user_id}/recipes">${comment.user_name}</a>`
+  let $commentString = $(`<p>${userLink}<br>${comment.text}</p>`)    
+  $('#comments').prepend($commentString)
 }
 
-function prependComment(){
-  console.log(response)
+function clearComments(){
+  $('#js-comment-div').remove()
+  $('#comments').empty()
+}
+
+// NOT IN USE
+function displayComment(commentString){
+  let $commentString = $(`<p>${comment.user_name}<br>${comment.text}</p>`)   
+  $('#comments').append($commentTemplate)
+}
+
+// NOT IN USE
+function hideComments(){
+  $('#js-comment-div').remove()
+  $('#show-comments').text("Hide Comments").on('click', clearComments)
+}
+
+// NOT IN USE
+function addCommentLink(){
+  let recipeId = $('#js-recipe-title').data().recipeId
+  let $commentLink = $("<a>",{
+    text: "Add A Comment",
+    "href": "#",
+    "id": "js-add-comment",
+    "data-recipe-id": recipeId,
+  }).on('click', addCommentField)
+
+  $('#show-comments').after($($commentLink))
 }
