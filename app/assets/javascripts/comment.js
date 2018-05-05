4024842536f8d949ec11ab58a82f345a5b75cb58
chrisjamesr <chrisjames.r@gmail.com>
2018-05-05 10:24:59 -0400

    showComments function receives correct json response from loadComments function

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,16 +1,36 @@
+$(document).on('turbolinks:load', function(){
+  addCommentEventListeners()
+})
 
+
+function addCommentEventListeners(){
+  $("#show-comments").on('click', loadComments)
+  $("#add-comment").on('click', addCommentField)
+}
+
+function addCommentField(){
+  event.stopPropagation()
+  let $commentField = $("<input>",{
+    "placeholder": "Add Comment...",
+    "type": "text",
+    "name": "comment"
+  })
+  // $('#comments')
+}
 function loadComments(){
-  const url;
+  event.stopPropagation()
+  const recipeId = this.dataset.recipeId
   $.ajax({
       method: "GET",
-      url: '/recipes/4/comments',
+      url: `/recipes/${recipeId}/comments`,
       dataType: 'json'
     }).done(showComments)
 }
 
 function postComment(){
-  const commentText;
-  const url;
+  
+  // const commentText;
+  // const url;
   $.ajax({
     method: "POST",
     url: '/recipes/4/comments',
@@ -20,10 +40,11 @@ function postComment(){
     
 }
 
-function showComments(){
-
+function showComments(commentsResponse){
+  debugger
+  console.log(commentsResponse)
 }
 
 function prependComment(){
-
+  console.log(response)
 }
