4545553e1b7b6072307fb7be0a85041f03c3244c
chrisjamesr <chrisjames.r@gmail.com>
2018-05-05 09:33:46 -0400

    remove associations to reduce size of post response, full recipe and user json models unnecessary

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,13 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :text, :recipe_id, :user_id
-  belongs_to :user
-  belongs_to :recipe
+  # belongs_to :user
+  # belongs_to :recipe
+
+  # def recipe_id
+  #   object.recipe.id
+  # end
+
+  # def user_id
+  #   object.user.id
+  # end
 end
