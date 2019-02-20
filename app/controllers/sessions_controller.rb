dfd3b7ae36188911e7388b2154c94ee52f652e7f
chrisjamesr <chrisjames.r@gmail.com>
2019-02-20 17:33:37 -0500

    modify session controller to expect :email fields

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,12 @@ class SessionsController < ApplicationController
     if auth.present?
       @user = User.login_from_omniauth(auth)
       set_session
-      redirect_to user_recipes_path(@user), :notice => "Welcome #{@user.name.capitalize}"
-    elsif params[:user][:name].present? && params[:user][:password].present? 
-      @user = User.find_by(:name => params[:user][:name])
+      redirect_to user_recipes_path(@user), :notice => "Welcome #{@user.username.capitalize}"
+    elsif params[:user][:email].present? && params[:user][:password].present? 
+      @user = User.find_by(:email => params[:user][:email])
       if @user && @user.authenticate(params[:user][:password])
         set_session
-        redirect_to user_recipes_path(@user), :notice => "Welcome #{@user.name.capitalize}"
+        redirect_to user_recipes_path(@user), :notice => "Welcome #{@user.username.capitalize}"
       else
         set_errors(params)
         render :new
@@ -37,8 +37,8 @@ class SessionsController < ApplicationController
 
   def set_errors(params)
 
-    flash.now[:alert] = {:name => "Name Required"} if params[:user][:name].nil?
-    flash.now[:alert] = {:name => "User Not Found"} if User.find_by(:name => params[:user][:name]).nil?
+    flash.now[:alert] = {:email => "Email Required"} if params[:user][:email].nil?
+    flash.now[:alert] = {:email => "User Not Found"} if User.find_by(:email => params[:user][:email]).nil?
     flash.now[:alert] = {:password => "Password Required"} if params[:user][:password].nil?
     flash.now[:alert] = {:password => "Password incorrect"} if @user && !@user.authenticate(params[:user][:password])
 
