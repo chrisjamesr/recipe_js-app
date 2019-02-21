b2009c081825e84b50c395e0f03a4ec1c529a546
chrisjamesr <chrisjames.r@gmail.com>
2019-02-20 22:19:22 -0500

    change username attribute to name

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,5 +1,5 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :username
+  attributes :id, :name
   has_many :recipe_categories
   has_many :recipes, through: :recipe_categories
 end
--- app/serializers/ingredient_serializer.rb
+++ app/serializers/ingredient_serializer.rb
@@ -1,4 +1,4 @@
 class IngredientSerializer < ActiveModel::Serializer
-  attributes :id, :username
+  attributes :id, :name
   has_many :recipe_ingredients
 end
