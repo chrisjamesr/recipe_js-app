a4dc735c00e307eb8fe02badabacdd0eb98efa88
chrisjamesr <chrisjames.r@gmail.com>
2019-02-24 21:48:08 -0500

    add attr accessor for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   validates :email, presence: true, allow_blank: false
   validates :email, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: { in: 6..20 }
+  attr_accessor :username
 
   def self.login_from_omniauth(auth)
     find_from_omniauth(auth) || create_from_omniauth(auth)
@@ -21,7 +22,7 @@ class User < ApplicationRecord
   end
 
   def username
-    @username = email.match(/.+(?=@)/)[0]    
+    @username = email.match(/.+(?=@)/)[0]     
   end 
 
 end
